3e911109c53d35ac8e626b5278331b700693b62a
Nilesh Pandey <32990329+nile649@users.noreply.github.com>
2019-07-25 18:54:07 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from torchvision.utils import save_image
 from torch.autograd import Variable
 from torchvision.utils import save_image
 from binascii import a2b_base64
+import base64
 
 def get_opt():
     parser = argparse.ArgumentParser()
@@ -16,16 +17,30 @@ def get_opt():
     return opt
 def get_img():
     cimg = open("./cimg.txt", "br")
+    cimg = cimg.read()
+    cimg = cimg[23:]
+
     eimg = open("./eimg.txt", "br")
-    binary_data = a2b_base64(cimg.read())
+    eimg = eimg.read()
+    eimg = eimg[23:]
+
+    binary_data = a2b_base64(cimg)
     fd = open('cimg.jpg', 'wb')
     fd.write(binary_data)
     fd.close()
-    binary_data = a2b_base64(eimg.read())
+    binary_data = a2b_base64(eimg)
     fd = open('eimg.jpg', 'wb')
     fd.write(binary_data)
     fd.close()
 
+def write_img():
+    encoded = base64.b64encode(open("./simage.jpg", "rb").read())
+    text_file = open("./simg.txt", "bw")
+    b = bytearray(encoded)
+    a = bytearray(b'data:image/jpeg;base64,')
+    encoded = a+b
+    text_file.write(encoded)
+    text_file.close()
 
 def get_res(opt):
     datalo = testdata()
@@ -46,7 +61,8 @@ def get_res(opt):
     pic = (torch.cat([gen], dim=0).data + 1) / 2.0
     save_dir = simage
     save_image(pic, '{}/simage.jpg'.format(save_dir), nrow=1)
-    
+    write_img()
+
 def main():
     opt = get_opt()
     get_res(opt)
